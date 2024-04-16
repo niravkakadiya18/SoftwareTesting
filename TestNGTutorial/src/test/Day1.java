8ffc67d9c5b55f9fa324036b163f48b2a195c64f
Nirav Kakadiya <141065523+niravkakadiya18@users.noreply.github.com>
2024-04-16 18:41:00 +0530

    Add files via upload

---
+++ TestNGTutorial/src/test/Day1.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day1 {
+
+	@Test
+
+	public void Demo() {
+		System.out.println("hello");
+	}
+	
+	@Test
+	public void SecondTest()
+	{
+		System.out.println("bye");
+	}
+
+}
---
+++ Udemy/src/MethodsDemo2.java
@@ -0,0 +1,20 @@
+
+public class MethodsDemo2 {
+
+	public static String getData2()
+
+	{
+
+	System.out.println ("hello world");
+
+	return "rahul shetty";
+
+	}
+
+	public void getUserData() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+	}
+
+}
